ede2da01717df902676c34cd74f68b3b530be013
jaeeunjeong <49487155+jaeeunjeong@users.noreply.github.com>
2022-02-04 19:34:02 +0900

    Update SelectionSort.java
    for문을 이용항 구현 추가.

--- Algorithm/SelectionSort.java
+++ Algorithm/SelectionSort.java
@@ -1,3 +1,4 @@
+
 /**
  * 가장 작은 것만 찾기위해 재귀/for문을 이용해서 값을 구한다.
  */
@@ -7,21 +8,20 @@ public class SelectionSort {
 	}
 
 	public static void selectionSortByRecursive(int[] arr, int start) {
-		if (start == arr.length)
+		if (arr.length == start)
 			return;
 
 		int min = arr[start];
 		int min_idx = start;
 		for (int i = start; i < arr.length; i++) {
 			int cur = arr[i];
-			if (min > cur) {
+			if (cur < min) {
 				min = cur;
 				min_idx = i;
 			}
 		}
 		swap(min_idx, start, arr);
 		selectionSortByRecursive(arr, start + 1);
-
 	}
 
 	public static void swap(int idx, int start, int[] arr) {
@@ -33,6 +33,7 @@ public class SelectionSort {
 	public static void print(int[] arr) {
 		for (int x : arr)
 			System.out.print(x + " ");
+		System.out.println();
 	}
 
 	public static void selectionSortByLoop(int[] arr) {
@@ -45,15 +46,15 @@ public class SelectionSort {
 					max = arr[j];
 				}
 			}
-			max = arr[i];// temp의 의미임
-			arr[i] = arr[idx];
-			arr[idx] = max;
+			swap(idx, i, arr);
 		}
 	}
 
 	public static void main(String[] args) {
 		int[] arr = { 20, 22, 1, 26, 11, 23 };
-		// init(arr);
+		print(arr);
+		init(arr);
+		print(arr);
 		selectionSortByLoop(arr);
 		print(arr);
 	}
